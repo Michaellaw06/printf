d46165f485936913fa05919203b0045ebad8c02e
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-18 00:05:18 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
@@ -10,10 +9,11 @@
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
+ *
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -24,10 +24,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-	{
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-	}
+
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
@@ -40,7 +39,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
-			if (fmt[*ind] == ' )
+			if (fmt[*ind] == ' ')
 				--(*ind);
 			return (1);
 		}

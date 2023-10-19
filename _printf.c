144b124d867777e7a1be6336eb7b7c1fa35e8a6e
Zai-nab23 <140807836+Zai-nab23@users.noreply.github.com>
2023-10-19 17:43:14 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,55 +2,50 @@
 
 void print_buffer(char buffer[], int *buff_ind);
 
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+    int i, total_printed_chars = 0;
+    int flags, width, precision, size, buff_index = 0;
+    va_list args;
+    char output_buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return -1;
 
-	va_start(list, format);
+    va_start(args, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+    for (i = 0; format && format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            output_buffer[buff_index++] = format[i];
+            if (buff_index == BUFF_SIZE)
+                print_buffer(output_buffer, &buff_index);
+            total_printed_chars++;
+        }
+        else
+        {
+            print_buffer(output_buffer, &buff_index);
+            flags = get_flags(format, &i);
+            width = get_width(format, &i, args);
+            precision = get_precision(format, &i, args);
+            size = get_size(format, &i);
+            ++i;
+            int chars_printed = handle_print(format, &i, args, output_buffer,
+                flags, width, precision, size);
+            if (chars_printed == -1)
+                return -1;
+            total_printed_chars += chars_printed;
+        }
+    }
 
-	print_buffer(buffer, &buff_ind);
+    print_buffer(output_buffer, &buff_index);
 
-	va_end(list);
+    va_end(args);
 
-	return (printed_chars);
+    return total_printed_chars;
 }
+
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
